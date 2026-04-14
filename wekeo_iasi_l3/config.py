3638aae1a7fee2a740b0ad01916054d56d0a9980
Joackim Orcière <joackim.orciere@hygeos.com>
2026-04-14 14:21:29 +0200

    Improved config

--- wekeo_iasi_l3/config.py
+++ wekeo_iasi_l3/config.py
@@ -1,9 +1,12 @@
 from wekeo_iasi_l3.hygeos_core import env
 output_dir = env.getdir("OUTPUT_DIR")
-iasi_download_dir = env.getdir("DIR_ANCILLARY") / "IASI_L2"
+ancillary_dir = env.getdir("DIR_ANCILLARY") 
 
-if not iasi_download_dir.exists():
-    raise FileNotFoundError(f"IASI download directory {iasi_download_dir} does not exist. Please create it or check your environment configuration.")
+if not ancillary_dir.exists():
+    raise FileNotFoundError(f"Ancillary directory {ancillary_dir} does not exist. Please check your environment configuration.")
+
+iasi_download_dir = ancillary_dir / "IASI_L2"
+iasi_download_dir.mkdir(parents=False, exist_ok=True)
 
 if not output_dir.exists():
     raise FileNotFoundError(f"Output directory {output_dir} does not exist. Please create it or check your environment configuration.")
